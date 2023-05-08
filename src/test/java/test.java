41536f66e06baccfd5c750ca2b3adfcd5884a2ac
ss13 <ssa0618@yandex.ru>
2023-05-08 14:26:42 +0300

    first commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class Etest {
+public class test {
     @Test
     void shouldReturnAccounts() {
         given()
