b2a7c167118e7f5a0cf19e9d800df227b02d3725
ss13 <ssa0618@yandex.ru>
2023-05-08 14:06:43 +0300

    1

---
+++ src/test/java/Etest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Etest {
+    @Test
+    void shouldReturnAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+
+    }
+}
